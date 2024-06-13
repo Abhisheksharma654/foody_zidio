17dac299fc3126164cb72e8c50f32e9a2358a1fa
Abhisheksharma654 <Abhisheksharma654@gmail.com>
2024-06-13 10:09:16 +0530

    intial_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:foody_zidio/pages/bottomnav.dart';
 import 'package:foody_zidio/pages/home.dart';
 import 'package:foody_zidio/pages/login.dart';
-
 
 void main() {
   runApp(const MainApp());
@@ -14,13 +12,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+         
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Foody',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const Home(),
+      home: BottomNav(),
     );
   }
 }
