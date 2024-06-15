e9ce914b800e7bd93358df214538bc32d6df82d3
Abhisheksharma654 <Abhisheksharma654@gmail.com>
2024-06-16 01:53:17 +0530

    initial_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
 
 import 'package:foody_zidio/pages/onboard.dart';
 import 'package:foody_zidio/pages/signup.dart';
-import 'package:foody_zidio/service/app_constraint.dart';
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Stripe.publishableKey = publishableKey;
   await Firebase.initializeApp();
   runApp(const MainApp());
 }
@@ -19,7 +15,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-         
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Foody',
