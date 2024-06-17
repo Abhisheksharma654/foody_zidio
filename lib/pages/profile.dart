6fc6cbe9c3cbfce64e4e5c186d0e4d2079206b01
ParthVadodariya2005 <parthmevada2005@gmail.com>
2024-06-17 15:46:10 +0530

    commit

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,11 +1,8 @@
-import 'dart:io';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:foody_zidio/Auth/auth_method.dart';
-import 'package:foody_zidio/service/shared_pref.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:random_string/random_string.dart';
- // Import the SharedPreferenceHelper from shared_pref.dart
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:foody_zidio/Content/onboard.dart';
+import 'package:foody_zidio/main.dart';
+
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -15,246 +12,24 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  String? profile, name, email;
-  final ImagePicker _picker = ImagePicker();
-  File? selectedImage;
-
-  Future getImage() async {
-    var image = await _picker.pickImage(source: ImageSource.gallery);
-
-    selectedImage = File(image!.path);
-    setState(() {
-      uploadItem();
-    });
-  }
-
-  uploadItem() async {
-    if (selectedImage != null) {
-      String addId = randomAlphaNumeric(10);
-
-      Reference firebaseStorageRef =
-          FirebaseStorage.instance.ref().child("blogImages").child(addId);
-      final UploadTask task = firebaseStorageRef.putFile(selectedImage!);
-
-      var downloadUrl = await (await task).ref.getDownloadURL();
-      await SharedPreferenceHelper().saveUserProfile(downloadUrl);
-      setState(() {
-        profile = downloadUrl;
-      });
-    }
-  }
-
-  getthesharedpref() async {
-    profile = await SharedPreferenceHelper().getUserProfile();
-    name = await SharedPreferenceHelper().getUserName();
-    email = await SharedPreferenceHelper().getUserEmail();
-    setState(() {});
-  }
-
-  onthisload() async {
-    await getthesharedpref();
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    onthisload();
-    super.initState();
+  Future<void> _logout() async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => Onboard()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: name == null
-          ? CircularProgressIndicator()
-          : Container(
-              child: Column(
-                children: [
-                  Stack(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(top: 45.0, left: 20.0, right: 20.0),
-                        height: MediaQuery.of(context).size.height / 4.3,
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                            color: Colors.black,
-                            borderRadius: BorderRadius.vertical(
-                                bottom: Radius.elliptical(
-                                    MediaQuery.of(context).size.width, 105.0))),
-                      ),
-                      Center(
-                        child: Container(
-                          margin: EdgeInsets.only(
-                              top: MediaQuery.of(context).size.height / 6.5),
-                          child: Material(
-                            elevation: 10.0,
-                            borderRadius: BorderRadius.circular(60),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(60),
-                              child: selectedImage == null
-                                  ? GestureDetector(
-                                      onTap: () {
-                                        getImage();
-                                      },
-                                      child: profile == null
-                                          ? Image.asset(
-                                              "images/boy.jpg",
-                                              height: 120,
-                                              width: 120,
-                                              fit: BoxFit.cover,
-                                            )
-                                          : Image.network(
-                                              profile!,
-                                              height: 120,
-                                              width: 120,
-                                              fit: BoxFit.cover,
-                                            ),
-                                    )
-                                  : Image.file(
-                                      selectedImage!,
-                                      height: 120,
-                                      width: 120,
-                                      fit: BoxFit.cover,
-                                    ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 70.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              name!,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 23.0,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Poppins'),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  buildInfoCard(Icons.person, "Name", name!),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  buildInfoCard(Icons.email, "Email", email!),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  buildInfoCard(Icons.description, "Terms and Condition", ""),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  buildActionCard(Icons.delete, "Delete Account", () {
-                     AuthMethods().deleteUser();
-                  }),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  buildActionCard(Icons.logout, "LogOut", () {
-                     AuthMethods().signOut();
-                  })
-                ],
-              ),
-            ),
-    );
-  }
-
-  Widget buildInfoCard(IconData icon, String label, String value) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 20.0),
-      child: Material(
-        borderRadius: BorderRadius.circular(10),
-        elevation: 2.0,
-        child: Container(
-          padding: EdgeInsets.symmetric(
-            vertical: 15.0,
-            horizontal: 10.0,
-          ),
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(10)),
-          child: Row(
-            children: [
-              Icon(
-                icon,
-                color: Colors.black,
-              ),
-              SizedBox(
-                width: 20.0,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    label,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                    value,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600),
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        title: Text('Profile'),
       ),
-    );
-  }
-
-  Widget buildActionCard(IconData icon, String label, VoidCallback onTap) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20.0),
-        child: Material(
-          borderRadius: BorderRadius.circular(10),
-          elevation: 2.0,
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 15.0,
-              horizontal: 10.0,
-            ),
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(10)),
-            child: Row(
-              children: [
-                Icon(
-                  icon,
-                  color: Colors.black,
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      label,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w600),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _logout,
+          child: Text('Logout'),
         ),
       ),
     );
