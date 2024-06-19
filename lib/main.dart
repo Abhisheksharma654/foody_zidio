c103d0a7cb2da85fa8631c7e74ae242cc898ab28
Abhisheksharma654 <Abhisheksharma654@gmail.com>
2024-06-20 00:59:46 +0530

    initial_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,28 @@
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:foody_zidio/Admin/admin.dart';
 import 'package:foody_zidio/Content/bottom_nav.dart';
 import 'package:foody_zidio/Content/onboard.dart';
-import 'package:foody_zidio/pages/login.dart';
 import 'package:foody_zidio/service/app_constraint.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'pages/login.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Stripe.publishableKey = publishableKey;
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
@@ -34,19 +36,19 @@ class SplashScreen extends StatelessWidget {
       future: _checkUserStatus(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Scaffold(
+          return const Scaffold(
             body: Center(child: CircularProgressIndicator()),
           );
         } else {
           bool isLoggedIn = snapshot.data?[0] as bool;
-         bool hasCompletedOnboarding = snapshot.data?[1] as bool;
+          bool hasCompletedOnboarding = snapshot.data?[1] as bool;
 
           if (isLoggedIn) {
             return BottomNav();
           } else if (hasCompletedOnboarding) {
-            return LogIn();
+            return const LogIn();
           } else {
-            return Onboard();
+            return const Onboard();
           }
         }
       },
