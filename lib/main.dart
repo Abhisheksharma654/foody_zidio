44b8a832ebac0e7313adebe79773e54f56ca353d
Parth Vadodariya <109171598+ParthVadodariya2005@users.noreply.github.com>
2024-06-24 21:27:46 +0530

    Update main.dart
    for stay logged in not to signoit when app is closed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:foody_zidio/Content/bottom_nav.dart';
-import 'package:foody_zidio/Content/onboard.dart';
 import 'package:foody_zidio/service/app_constraint.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'pages/login.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,26 +37,20 @@ class SplashScreen extends StatelessWidget {
             body: Center(child: CircularProgressIndicator()),
           );
         } else {
-          bool isLoggedIn = snapshot.data?[0] as bool;
-          bool hasCompletedOnboarding = snapshot.data?[1] as bool;
+          bool isLoggedIn = snapshot.data as bool;
 
           if (isLoggedIn) {
             return BottomNav();
-          } else if (hasCompletedOnboarding) {
-            return const LogIn();
           } else {
-            return const Onboard();
+            return const LogIn();
           }
         }
       },
     );
   }
 
-  Future<List<bool>> _checkUserStatus() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool hasCompletedOnboarding = prefs.getBool('onboardingComplete') ?? false;
+  Future<bool> _checkUserStatus() async {
     User? user = FirebaseAuth.instance.currentUser;
-    bool isLoggedIn = user != null;
-    return [isLoggedIn, hasCompletedOnboarding];
+    return user != null;
   }
 }
