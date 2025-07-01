5e9ea0693654355a28e7013ea7c0d799962dd4ab
VP7985 <parthmevada2005@gmail.com>
2025-07-01 16:51:34 +0530

    widget_define

---
+++ lib/services/widget_support.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class AppWidget{
+
+  static TextStyle boldTextFeildStyle(){
+    return  const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Poppins');
+  }
+
+    static TextStyle HeadlineTextFeildStyle(){
+    return  const TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Poppins');
+  }
+  static TextStyle HeadlineText1FeildStyle(){
+    return  const TextStyle(
+                  color: Colors.black,
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Poppins');
+  }
+
+      static TextStyle LightTextFeildStyle(){
+      return  const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Poppins');
+  }
+
+        static TextStyle semiBoldTextFeildStyle(){
+        return  const TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Poppins');
+  }
+   static TextStyle semiBoldblackTextFeildStyle(){
+        return  const TextStyle(
+                  color: Colors.black,
+                  fontSize: 29.0,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Poppins');
+  }
+  static TextStyle semiBoldWhiteTextFeildStyle(){
+        return  const TextStyle(
+                  color: Colors.white,
+                  fontSize: 23.0,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Poppins');
+  }
+   static TextStyle semiBoldWhite1TextFeildStyle(){
+        return  const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Poppins');
+  }
+}
